9216be74d2f0874a3203a82e45217a13d03dbc06
梁显杰 <liangxianjie>
2018-07-19 10:09:44 +0800

    Initial Commit

---
+++ F-Jiege/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  F-Jiege
+//
+//  Created by zhongding on 2018/7/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ F-Jiege/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  F-Jiege
+//
+//  Created by zhongding on 2018/7/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
